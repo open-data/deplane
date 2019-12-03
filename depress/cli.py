b06f95f9fe04e300eb0204051dae16c13c29e6a7
Ian Ward <ian@excess.org>
2019-12-03 16:45:20 -0500

    generate example docx

--- depress/cli.py
+++ depress/cli.py
@@ -1,5 +1,12 @@
 import click
+import requests
+
+from depress.publish import write_docx
 
 @click.command()
-def cli():
-    click.echo('hi')
+@click.argument('lang')
+@click.argument('url')
+@click.argument('filename')
+def cli(lang, url, filename):
+    geno = requests.get(url)
+    write_docx(filename)
