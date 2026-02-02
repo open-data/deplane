64bf70e48cb09691a5c9e440583b10937ddcb5b0
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2026-02-02 16:50:02 +0000

    feat(dev): github yaml support, nap support;
    - Added in ability to load more advanced Schema stuff from github yamls.
    - Added in special functionality for National Action Plans.

--- deplane/cli.py
+++ deplane/cli.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import gettext
 
+from typing import Optional
+
 import yaml
 import click
 import requests
@@ -13,26 +15,41 @@ from deplane.publish import write_docx
 @click.argument('lang', type=click.Choice(['en', 'fr']))
 @click.argument('url')
 @click.argument('filename', type=click.Path())
-def cli(lang, url, filename):
+@click.option('-g', '--github', type=click.STRING,
+              help='Github branch of open-data/ckanext-canada to use' \
+                   ' as the remote YAML Schema for more advanced DEPs.')
+def cli(lang, url, filename, github: Optional[str] = None):
     """
     Create Data Element Profile docx based on ckanext-recombinant json schema
     """
-    cli_impl(lang, url, filename)
+    cli_impl(lang, url, filename, github)
 
 
-def cli_impl(lang, url, filename):
+def cli_impl(lang, url, filename, github):
+    print('Using translation file for language: %s' % lang)
+    french_trans = gettext.translation(
+        'deplane',
+        Path(__file__).parent / 'i18n',
+        languages=['fr'],
+    )
+    french_trans.install()
     if lang == 'en':
         trans = gettext.NullTranslations()
     else:
-        trans = gettext.translation(
-            'deplane',
-            Path(__file__).parent / 'i18n',
-            languages=[lang],
-        )
-        trans.install()
+        trans = french_trans
+    if github:
+        t = url.rpartition('/')[-1].replace('.json', '')
+        r = f'https://raw.githubusercontent.com/open-data/ckanext-canada/refs/heads/{github}/ckanext/canada/tables/{t}.yaml'
+        print('Fetching geno for %s from Github remote: %s' % (t, r))
+        f = requests.get(r).content
+        geno = yaml.safe_load(f)
+        write_docx(geno, filename, trans, lang, french_trans)
+        return
     try:
         geno = requests.get(url).json()
+        print('Fetching geno from remote JSON: %s' % url)
     except (InvalidSchema, MissingSchema):
         with open(url) as f:
             geno = yaml.safe_load(f)
-    write_docx(geno, filename, trans, lang)
+        print('Fetching geno from file: %s' % url)
+    write_docx(geno, filename, trans, lang, french_trans)
--- deplane/publish.py
+++ deplane/publish.py
@@ -1,10 +1,15 @@
 from pathlib import Path
 from contextlib import contextmanager
+import re
 
 # noinspection PyPackageRequirements
 from docx import Document
 # noinspection PyPackageRequirements
 from docx.shared import Cm
+# noinspection PyPackageRequirements
+from docx.oxml.ns import qn
+# noinspection PyPackageRequirements
+from docx.oxml import OxmlElement
 
 from deplane.md_to_docx import insert_markdown, format_table
 
@@ -43,8 +48,10 @@ STRINGS = [
     _('Describes the condition or conditions according to which a value shall be present in French. Indicates what the system will accept in this field.')
 ]
 
+NAP_MATCH = re.compile(r'^nap(\d)*$')
 
-def write_docx(schema, filename, trans, lang):
+
+def write_docx(schema, filename, trans, lang, french_trans):
     """
     :param schema: recombinant-schema json dict
     :param filename: output docx filename to create
@@ -56,7 +63,9 @@ def write_docx(schema, filename, trans, lang):
     document = Document(Path(__file__).parent / 'ENG_2_Colour.docx')
     section = document.sections[0]
 
-    title = schema['title'][lang]
+    title = schema['title']
+    if isinstance(title, dict):
+        title = title[lang]
 
     section.different_first_page_header_footer = True
     section.first_page_header.paragraphs[0].text = ''
@@ -92,7 +101,7 @@ def write_docx(schema, filename, trans, lang):
             document,
             [Cm(4.69), Cm(11.80)],
             top_color='d9d9d9',
-            left_color='c6d9f1') as (trow, mrow):
+            left_color='c6d9f1') as (trow, mrow, ttable):
 
         trow(_('Attribute'), _('Attribute Description'))
         trow(
@@ -157,7 +166,7 @@ Indicates what the system will accept in this field.'''),
     with build_table(
             document,
             [Cm(3.69), Cm(6.4), Cm(6.4)],
-            top_color='d9d9d9') as (trow, mrow):
+            top_color='d9d9d9') as (trow, mrow, ttable):
         trow(_('Code'), _('English'), _('French'))
         trow('CODE1', _('English Description 1'), _('French Description 1'))
         trow('CODE2', _('English Description 2'), _('French Description 2'))
@@ -165,47 +174,146 @@ Indicates what the system will accept in this field.'''),
     document.add_page_break()
 
     for rnum, res in enumerate(schema['resources'], 1):
-        document.add_heading(res['title'][lang] + '\n', 1)
+        res_title = schema['title']
+        if isinstance(res_title, dict):
+            res_title = res_title[lang]
+
+        document.add_heading(res_title + '\n', 1)
+
+        rname = res.get('resource_name', '')
+        is_national_action_plan = NAP_MATCH.match(rname) is not None
 
         for fnum, field in enumerate(res['fields'], 1):
-            document.add_heading(f'{rnum}-{fnum} {field["label"][lang]}\n', 2)
             typ = field.get('type', field.get('datastore_type'))
+            fid = field.get('id', field.get('datastore_id'))
+            is_pub_field = field.get('published_resource_computed_field', False)
+            if not field.get('import_template_include', True):
+                continue
+            if not field.get('visible_to_public', True):
+                continue
+            if fid == 'owner_org' or fid == 'owner_org_title':
+                continue
+
+            document.add_heading(f'{rnum}-{fnum} {field["label"][lang]}\n', 2)
+
             with build_table(
                     document,
                     [Cm(4.69), Cm(11.80)],
                     top_color='d9d9d9',
-                    left_color='c6d9f1') as (trow, mrow):
+                    left_color='c6d9f1') as (trow, mrow, ttable):
+                
                 trow(_('Attribute'), _('Attribute Description'))
+
                 trow(_('Field Name EN'), field['label']['en'])
                 trow(_('Field Name FR'), field['label']['fr'])
-                trow(_('ID'), field['id'])
+
+                trow(_('ID'), fid)
+
                 mrow(_('Description EN'), field.get('description', {}).get('en', ''))
                 mrow(_('Description FR'), field.get('description', {}).get('fr', ''))
-                mrow(_('Obligation EN'), field.get('obligation', {}).get('en', trans.gettext(OBLIGATION[field.get('obligation', 'optional')]) if not isinstance(field.get('obligation'), dict) and field.get('obligation') in OBLIGATION else ''))
-                mrow(_('Obligation FR'), field.get('obligation', {}).get('fr', trans.gettext(OBLIGATION[field.get('obligation', 'optional')]) if not isinstance(field.get('obligation'), dict) and field.get('obligation') in OBLIGATION else ''))
+
+                # JSON Schema vs YAML Schema obligations gets a bit complicated...
+                obligation_en = field.get('obligation', 'optional')
+                if not isinstance(obligation_en, dict) and obligation_en in OBLIGATION:
+                    obligation_en = trans.gettext(OBLIGATION[obligation_en])
+                elif isinstance(obligation_en, dict):
+                    obligation_en = obligation_en.get('en')
+                    if obligation_en in OBLIGATION:
+                        obligation_en = trans.gettext(OBLIGATION[obligation_en])
+                    else:
+                        obligation_en = trans.gettext(obligation_en)
+                if not obligation_en:
+                    obligation_en = trans.gettext('Optional')
+                obligation_fr = field.get('obligation', 'optional')
+                if not isinstance(obligation_fr, dict) and obligation_fr in OBLIGATION:
+                    obligation_fr = french_trans.gettext(OBLIGATION[obligation_fr])
+                elif isinstance(obligation_fr, dict):
+                    obligation_fr = obligation_fr.get('fr')
+                    if obligation_fr in OBLIGATION:
+                        obligation_fr = french_trans.gettext(OBLIGATION[obligation_fr])
+                    else:
+                        obligation_fr = french_trans.gettext(obligation_fr)
+                elif isinstance(obligation_fr, str) and obligation_fr in OBLIGATION.values():
+                    obligation_fr = french_trans.gettext(obligation_fr)
+                if not obligation_fr:
+                    obligation_fr = french_trans.gettext('Optional')
+                mrow(_('Obligation EN'), obligation_en)
+                mrow(_('Obligation FR'), obligation_fr)
+
                 trow(_('Occurrence'), trans.gettext(field.get('occurrence', 'Single')))
+
                 mrow(_('Format Type'), trans.gettext(FORMAT_TYPE[typ]))
+
                 trow(_('Validation EN'), field.get('validation', {}).get('en', ''))
                 trow(_('Validation FR'), field.get('validation', {}).get('fr', ''))
+
                 if field.get('character_limit', field.get('max_chars')):
                     trow(_('Character Limit'), str(field.get('character_limit', field.get('max_chars'))))
-                eg = res['example_record'].get(field['id'], '')
-                if isinstance(eg, list):
-                    eg = ','.join(eg)
-                trow(_('Example Value'), str(eg))
+
+                eg_r = res.get('example_record' ,res.get('examples', {}).get('record', None))
+                if not is_pub_field and eg_r.get(fid):
+                    eg = eg_r[fid]
+                    if isinstance(eg, list):
+                        eg = ','.join(eg)
+                    trow(_('Example Value'), str(eg))
+
+            # TODO: choices_file
+            # if 'choices_file' in field:
+                
 
             if 'choices' in field:
                 document.add_paragraph(_('\nControlled List Values:'))
                 with build_table(
                         document,
                         [Cm(3.69), Cm(6.4), Cm(6.4)],
-                        top_color='d9d9d9') as (trow, mrow):
+                        top_color='d9d9d9') as (trow, mrow, ttable):
                     trow(_('Code'), _('English'), _('French'))
                     for c, v in field['choices'].items():
                         if isinstance(v, dict):
                             trow(c, v.get('en', ''), v.get('fr', ''))
                         else:
                             trow(c, v, v)
+                        # Special stuff for National Action Plans
+                        if (
+                          is_national_action_plan and 
+                          isinstance(v, dict) and
+                          (
+                           'due_date' in v or
+                           'deadline' in v or
+                           'lead_dept' in v or
+                           's4d' in v  
+                          )
+                        ):
+                            with ttable([Cm(3.69), Cm(12.8)], top_color='ddd8e8', left_color='e3e3e3') as innerT:
+                                cells = innerT.add_row().cells
+                                cells[0].text = _('Extras for: %s') % c
+                                cells[1].text = _('Extra Value')
+
+                                cells = innerT.add_row().cells
+                                cells[0].text = _('Due Date')
+                                default = 'TODO - supply value (must be one of reporting_period values)'
+                                cells[1].text = v.get('due_date') or default
+
+                                cells = innerT.add_row().cells
+                                cells[0].text = _('Deadline (English)')
+                                default = 'TODO - supply value (semantics e.g. "First of June 2026")'
+                                cells[1].text = v.get('deadline', {}).get('en') or default
+
+                                cells = innerT.add_row().cells
+                                cells[0].text = _('Deadline (French)')
+                                default = 'TODO - supply value (semantics e.g. "First of June 2026")'
+                                cells[1].text = v.get('deadline', {}).get('fr') or default
+
+                                cells = innerT.add_row().cells
+                                cells[0].text = _('Lead Department')
+                                default = 'TODO - supply value (must be organization abbreviation found here: https://open.canada.ca/data/en/api/action/organization_list)'
+                                cells[1].text = v.get('lead_dept') or default
+
+                                cells = innerT.add_row().cells
+                                cells[0].text = _('Summit for Democracy')
+                                default = 'TODO - supply value (must be "true" or "false")'
+                                cells[1].text = v.get('s4d') or default
+                        # :END: Special stuff for National Action Plans :END:
 
             document.add_paragraph('\n\n')
 
@@ -245,6 +353,47 @@ def build_table(document, widths, **format_args):
                 delete_paragraph(cells[i].paragraphs[0])
             insert_markdown(cells[i], c)
 
-    yield trow, mrow
+    @contextmanager
+    def ttable(inner_widths, **inner_format_args):
+        """Add a nested table"""
+        cells = table.add_row().cells
+        cell_1 = cells[0]
+        cell_n = cells[-1]
+        merged_cell = cell_1.merge(cell_n)
+        merged_cell._element.clear_content()  # clear leading paragraph
+        innerTable = merged_cell.add_table(rows=0, cols=len(inner_widths))
+        set_cell_margins(merged_cell,
+                         top=Cm(0),
+                         bottom=Cm(0),
+                         start=Cm(0),
+                         end=Cm(0))
+        yield innerTable
+        format_table(innerTable, widths=inner_widths, **inner_format_args)
+
+    yield trow, mrow, ttable
 
     format_table(table, widths=widths, **format_args)
+
+
+def set_cell_margins(cell, **kwargs):
+    """
+    Set the margins for a table cell.
+    
+    Values are specified in twentieths of a point (1/1440 of a centimeter).
+    Use the docx.shared.Cm for convenience.
+
+    Usage:
+    set_cell_margins(cell, top=Cm(0), bottom=Cm(0), start=Cm(0), end=Cm(0))
+    """
+    tc = cell._tc
+    tcPr = tc.get_or_add_tcPr()
+    tcMar = OxmlElement('w:tcMar')
+    for m in ["top", "start", "bottom", "end"]:
+        if m in kwargs:
+            node = OxmlElement(f"w:{m}")
+            # Ensure the value is an integer representing twentieths of a point
+            value_dxa = int(kwargs[m].emu / 6350)
+            node.set(qn('w:w'), str(value_dxa))
+            node.set(qn('w:type'), 'dxa')
+            tcMar.append(node)
+    tcPr.append(tcMar)
